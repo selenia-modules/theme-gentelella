9f754b7578e08fcfc4bbeff66a87ffcb02f98e9f
Cláudio Silva <claudio.silva@impactwave.com>
2016-10-31 18:30:55 +0000

    Upgrade to new bootstrap format.

--- src/Config/ThemeGentelellaModule.php
+++ src/Config/ThemeGentelellaModule.php
@@ -1,23 +1,25 @@
 <?php
 namespace Selenia\Themes\Gentelella\Config;
 
+use Electro\Core\Assembly\Services\Bootstrapper;
 use Electro\Core\Assembly\Services\ModuleServices;
 use Electro\Interfaces\ModuleInterface;
 use Selenia\Themes\Gentelella\Components\SideBarMenu;
 
 class ThemeGentelellaModule implements ModuleInterface
 {
-  function configure (ModuleServices $module)
+  static function boot (Bootstrapper $boot)
   {
-    $module
-      ->provideViews ()
-      ->provideMacros ()
-      ->registerComponents ([
-        'SideBarMenu' => SideBarMenu::class,
-      ])
-      // DO NOT IMPORT THE FOLLOWING NAMESPACE!
-      ->registerControllersNamespace (\Selenia\Platform\Components::class, 'platform')
-    ;
+    $boot->on (Bootstrapper::EVENT_BOOT, function (ModuleServices $module) {
+      $module
+        ->provideViews ()
+        ->provideMacros ()
+        ->registerComponents ([
+          'SideBarMenu' => SideBarMenu::class,
+        ])
+        // DO NOT IMPORT THE FOLLOWING NAMESPACE!
+        ->registerControllersNamespace (\Selenia\Platform\Components::class, 'platform');
+    });
   }
 
 }
