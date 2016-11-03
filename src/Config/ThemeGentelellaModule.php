2ae70fd460c2366fabf77a26cf6aade9984b8a1b
Cláudio Silva <claudio.silva@impactwave.com>
2016-11-03 19:19:47 +0000

    Module configuration refactoring.

--- src/Config/ThemeGentelellaModule.php
+++ src/Config/ThemeGentelellaModule.php
@@ -1,29 +1,29 @@
 <?php
 namespace Selenia\Themes\Gentelella\Config;
 
+use Electro\Interfaces\KernelInterface;
 use Electro\Interfaces\ModuleInterface;
 use Electro\Kernel\Lib\ModuleInfo;
-use Electro\Kernel\Services\Bootstrapper;
 use Electro\Plugins\Matisse\Config\MatisseSettings;
 use Electro\ViewEngine\Config\ViewEngineSettings;
 use Selenia\Themes\Gentelella\Components\SideBarMenu;
-use const Electro\Kernel\Services\CONFIGURE;
 
 class ThemeGentelellaModule implements ModuleInterface
 {
-  static function bootUp (Bootstrapper $bootstrapper, ModuleInfo $moduleInfo)
+  static function startUp (KernelInterface $kernel, ModuleInfo $moduleInfo)
   {
-    $bootstrapper->on (CONFIGURE, function (MatisseSettings $matisseSettings, ViewEngineSettings $viewEngineSettings)
-    use ($moduleInfo) {
-      $matisseSettings
-        ->registerMacros ($moduleInfo)
-        ->registerComponents ([
-          'SideBarMenu' => SideBarMenu::class,
-        ])
-        // DO NOT IMPORT THE FOLLOWING NAMESPACE!
-        ->registerControllersNamespace ($moduleInfo, \Selenia\Platform\Components::class, 'platform');
-      $viewEngineSettings->registerViews ($moduleInfo);
-    });
+    $kernel->onConfigure (
+      function (MatisseSettings $matisseSettings, ViewEngineSettings $viewEngineSettings)
+      use ($moduleInfo) {
+        $matisseSettings
+          ->registerMacros ($moduleInfo)
+          ->registerComponents ([
+            'SideBarMenu' => SideBarMenu::class,
+          ])
+          // DO NOT IMPORT THE FOLLOWING NAMESPACE!
+          ->registerControllersNamespace ($moduleInfo, \Selenia\Platform\Components::class, 'platform');
+        $viewEngineSettings->registerViews ($moduleInfo);
+      });
   }
 
 }
