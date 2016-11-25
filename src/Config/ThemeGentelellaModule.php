4c3671d41ca256c67b1fcdd0e85c6ba79e36462c
Cláudio Silva <claudio.silva@impactwave.com>
2016-11-25 22:57:05 +0000

    New bootstrapping rules and changes to configuration profiles (getCompatibleProfiles).

--- src/Config/ThemeGentelellaModule.php
+++ src/Config/ThemeGentelellaModule.php
@@ -5,11 +5,17 @@ use Electro\Interfaces\KernelInterface;
 use Electro\Interfaces\ModuleInterface;
 use Electro\Kernel\Lib\ModuleInfo;
 use Electro\Plugins\Matisse\Config\MatisseSettings;
+use Electro\Profiles\WebProfile;
 use Electro\ViewEngine\Config\ViewEngineSettings;
 use Selenia\Themes\Gentelella\Components\SideBarMenu;
 
 class ThemeGentelellaModule implements ModuleInterface
 {
+  static function getCompatibleProfiles ()
+  {
+    return [WebProfile::class];
+  }
+
   static function startUp (KernelInterface $kernel, ModuleInfo $moduleInfo)
   {
     $kernel->onConfigure (
