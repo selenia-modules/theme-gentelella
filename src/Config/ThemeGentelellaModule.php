e825ed59a4b1259a7b0f8504f3ba58d0da195418
Paulo Elias <paulo.cgelias@gmail.com>
2017-12-12 18:38:02 +0000

    Make theme only compatible with BackofficeProfile

--- src/Config/ThemeGentelellaModule.php
+++ src/Config/ThemeGentelellaModule.php
@@ -1,36 +1,37 @@
 <?php
+
 namespace Selenia\Themes\Gentelella\Config;
 
+use Agrozapp\Backoffice\Config\BackofficeProfile;
 use Electro\Interfaces\KernelInterface;
 use Electro\Interfaces\ModuleInterface;
 use Electro\Kernel\Lib\ModuleInfo;
-use Matisse\Config\MatisseSettings;
-use Electro\Profiles\WebProfile;
 use Electro\ViewEngine\Config\ViewEngineSettings;
+use Matisse\Config\MatisseSettings;
 use Selenia\Themes\Gentelella\Components\SideBarMenu;
 
 class ThemeGentelellaModule implements ModuleInterface
 {
-  static function getCompatibleProfiles ()
-  {
-    return [WebProfile::class];
-  }
+	static function getCompatibleProfiles()
+	{
+		return [BackofficeProfile::class];
+	}
 
-  static function startUp (KernelInterface $kernel, ModuleInfo $moduleInfo)
-  {
-    $kernel->onConfigure (
-      function (MatisseSettings $matisseSettings, ViewEngineSettings $viewEngineSettings)
-      use ($moduleInfo) {
-        $matisseSettings
-          ->registerMacros ($moduleInfo)
-          ->registerComponents ([
-            'SideBarMenu' => SideBarMenu::class,
-          ])
-					->registerPresets([ThemeGentelellaPresets::class])
-          // DO NOT IMPORT THE FOLLOWING NAMESPACE!
-          ->registerControllersNamespace ($moduleInfo, \Selenia\Platform\Components::class, 'platform');
-        $viewEngineSettings->registerViews ($moduleInfo);
-      });
-  }
+	static function startUp(KernelInterface $kernel, ModuleInfo $moduleInfo)
+	{
+		$kernel->onConfigure(
+				function (MatisseSettings $matisseSettings, ViewEngineSettings $viewEngineSettings)
+				use ($moduleInfo) {
+					$matisseSettings
+							->registerMacros($moduleInfo)
+							->registerComponents([
+									'SideBarMenu' => SideBarMenu::class,
+							])
+							->registerPresets([ThemeGentelellaPresets::class])
+							// DO NOT IMPORT THE FOLLOWING NAMESPACE!
+							->registerControllersNamespace($moduleInfo, \Selenia\Platform\Components::class, 'platform');
+					$viewEngineSettings->registerViews($moduleInfo);
+				});
+	}
 
 }
